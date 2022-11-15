230bc50adc31f8e79a45c6e1eb015e965fba1266
Iftekhar03 <97311559+Iftekhar03@users.noreply.github.com>
2022-11-15 08:12:23 +0600

    Add files via upload
    Problem
    =>You and your friend are playing a game with hoops. There are NN hoops (where NN is odd) in a row. You jump into hoop 11, and your friend jumps into hoop NN. Then you jump into hoop 22, and after that, your friend jumps into hoop N-1N−1, and so on.
    
    The process ends when someone cannot make the next jump because the hoop is occupied by the other person. Find the last hoop that will be jumped into.
    
    ###Input
    1. The first line contains an integer T, the number of test cases. Then the test cases follow.
    2. Each test case contains a single line of input, a single integer N.
    
    ###Output For each testcase, output in a single line the answer to the problem.

---
+++ upload.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void) {
+	int t,n,x,y,z,count,i,j,a,b,c;
+	scanf("%d",&t);
+	while(t--)
+	{
+	  scanf("%d",&a);
+	  a=(a/2)+1;
+    printf("%d\n",a);
+	}
+	return 0;
+  }
